44eb415a57d177573489e065a2c6a91a40d128d2
ext_ograzhdanova <ext_ograzhdanova@rencredit.ru>
2021-03-02 10:28:52 +0300

    test commit

--- src/main/java/com/fluffy/setdatewindows/Application.java
+++ src/main/java/com/fluffy/setdatewindows/Application.java
@@ -3,9 +3,7 @@ package com.fluffy.setdatewindows;
 public class Application {
 
     public static void main(String[] args) {
-
         WindowsSetDateTime windowsSetDateTime = new WindowsSetDateTime();
-
         windowsSetDateTime.SetLocalTime((short)2021, (short)03,(short) 01,(short) 19,(short) 30,(short) 0);
     }
 
