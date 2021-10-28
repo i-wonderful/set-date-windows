cced51391367de6ad897d663ab75f2b08a6fbab7
ext_ograzhdanova <ext_ograzhdanova@rencredit.ru>
2021-10-28 18:51:52 +0300

    Будь как дома, путник

--- src/main/java/com/fluffy/setdatewindows/Application.java
+++ src/main/java/com/fluffy/setdatewindows/Application.java
@@ -1,10 +1,22 @@
 package com.fluffy.setdatewindows;
 
+import java.time.LocalDateTime;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        LocalDateTime ldt = LocalDateTime.now();
+        short hour = 18;
+        short minute = 45;
         WindowsSetDateTime windowsSetDateTime = new WindowsSetDateTime();
-        windowsSetDateTime.SetLocalTime((short)2021, (short)03,(short) 01,(short) 19,(short) 30,(short) 0);
+        windowsSetDateTime.SetLocalTime(
+                (short)ldt.getYear(),
+                (short)ldt.getMonth().getValue(),
+                (short) ldt.getDayOfMonth(),
+                hour,
+                minute,
+                (short) 0);
     }
 
 }
